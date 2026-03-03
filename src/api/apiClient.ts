fdbc67d5f2f51657aa7c81da68ecc4ae83590124
MatiFasu <matiasfasulino@gmail.com>
2026-03-03 13:00:39 -0300

    Configuración de API_URL para Railway y fix de TypeScript para Netlify

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Cambia esta URL por la que te dé Railway o Render cuando hagas el deploy
-const API_URL = 'http://localhost:8080'; 
+const API_URL = 'https://shimmering-youthfulness-production.up.railway.app'; 
 
 const api = axios.create({
   baseURL: API_URL,
